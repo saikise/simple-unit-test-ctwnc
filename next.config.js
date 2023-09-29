e99b1191eb7d94fda1644ac983029d9b9fca2204
sai kise <134133636+saikise@users.noreply.github.com>
2023-09-28 22:06:20 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     eslint: {
         // Reference: https://nextjs.org/docs/app/building-your-application/configuring/eslint#linting-custom-directories-and-files
-        // Include __tests__ in the default list of directories.
+        // Include cypress in the default list of directories.
         dirs: ["pages", "app", "components", "lib", "src", "cypress"],
     },
 };
